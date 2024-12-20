535819a8ceca2e52c060b9fecdecf73d3599b7bc
pravesh dav <humantorch1932@gmail.com>
2024-12-20 20:45:24 +0530

    created gallery section

--- src/Components/Landing/GalleryMain.jsx
+++ src/Components/Landing/GalleryMain.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useRef } from 'react';
 import data from './GalleryData.js';
+import { FaChevronLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import './FeedbackSlider.css';
@@ -7,11 +10,13 @@ import { Autoplay, Navigation } from 'swiper/modules';
 
 function GalleryMain() {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedGallery, setSelectedGallery] = useState(null);
   const prevRef = useRef(null);
   const nextRef = useRef(null);
 
-  const handleGallery = () => {
-    setIsOpen(!isOpen);
+  const handleGallery = (gallery) => {
+    setIsOpen(true);
+    setSelectedGallery(gallery);
   };
 
   return (
@@ -30,7 +35,7 @@ function GalleryMain() {
             <p className="font-poppins text-xs lg:text-base">{item.description}</p>
             <button
               className={`self-${item.buttonAlignment} font-poppins capitalize italic text-primary border-t-2 border-b-2 border-primary text-sm py-1 px-7 my-5 lg:my-7 lg:text-xl lg:py-2`}
-              onClick={handleGallery}
+              onClick={() => handleGallery(item.galleryImgs)}
             >
               {item.button}
             </button>
@@ -39,10 +44,10 @@ function GalleryMain() {
       ))}
       {isOpen && (
         <div className="w-full h-screen bg-black/80 backdrop-blur-md fixed top-0 left-0 z-[100] flex justify-center items-center">
-          <button className="text-3xl absolute right-14 top-14 text-white" onClick={handleGallery}>
+          <button className="text-3xl absolute right-14 top-14 text-primary" onClick={() => setIsOpen(false)}>
             X
           </button>
-          <div className="w-80 h-96 overflow-hidden relative md:w-[45rem] md:h-[40rem]">
+          <div className="w-80 h-96 overflow-hidden relative lg:w-[45rem] lg:h-[40rem]">
             <Swiper
               className="mySwiper"
               modules={[Autoplay, Navigation]}
@@ -59,34 +64,36 @@ function GalleryMain() {
                 swiper.params.navigation.nextEl = nextRef.current;
               }}
             >
-              {data.map((item) =>
-                item.galleryImgs.map((img, imgIndex) => (
-                  <SwiperSlide
-                    key={imgIndex}
-                    className={`flex items-center justify-center transition-opacity duration-1000`}
-                  >
-                    {img.type === 'img' ? (
-                      <div className="w-full h-full bg-teal-600">
-                        <img src={img.url} alt={`Gallery Image ${imgIndex + 1}`} className="w-full h-full object-cover" />
-                      </div>
-                    ) : (
-                      <div className="w-full h-full bg-teal-600">
-                        <video src={img.url} autoPlay loop muted className="w-full h-full object-cover"></video>
-                      </div>
-                    )}
-                  </SwiperSlide>
-                ))
-              )}
+              {selectedGallery.map((img, imgIndex) => (
+                <SwiperSlide
+                  key={imgIndex}
+                  className={`flex items-center justify-center transition-opacity duration-1000`}
+                >
+                  {img.type === 'img' ? (
+                    <div className="w-full h-full bg-teal-600">
+                      <img src={img.url} alt={`Gallery Image ${imgIndex + 1}`} className="w-full h-full object-cover" />
+                    </div>
+                  ) : (
+                    <div className="w-full h-full bg-teal-600">
+                      <video src={img.url} autoPlay loop muted className="w-full h-full object-cover"></video>
+                    </div>
+                  )}
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
-          <div
-            ref={prevRef}
-            className="swiper-button-prev absolute left-0 top-1/2 -translate-y-1/2 z-10 text-white text-3xl cursor-pointer"
-          ></div>
-          <div
-            ref={nextRef}
-            className="swiper-button-next absolute right-0 top-1/2 -translate-y-1/2 z-10 text-white text-3xl cursor-pointer"
-          ></div>
+            <button
+              ref={prevRef}
+              className="custom-prev-button absolute left-20 top-[90%] lg:top-1/2 -translate-y-1/2 z-[100] border-2 border-primary text-primary text-2xl p-3 rounded-full cursor-pointer"
+            >
+              <FaChevronLeft />
+            </button>
+            <button
+              ref={nextRef}
+              className="custom-next-button absolute right-20 top-[90%] lg:top-1/2 -translate-y-1/2 z-[100] border-2 border-primary text-primary text-2xl p-3 rounded-full cursor-pointer"
+            >
+              <FaAngleRight />
+            </button>
         </div>
       )}
     </div>
