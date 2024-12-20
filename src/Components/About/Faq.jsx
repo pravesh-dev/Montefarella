9f6258b5c4100cd331c29a2017068d21d7bfc803
pravesh dav <humantorch1932@gmail.com>
2024-12-20 15:31:17 +0530

    faq completed

--- src/Components/About/Faq.jsx
+++ src/Components/About/Faq.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import FaqData from './FaqData'
 
 function Faq() {
   return (
-    <div className='w-full h-screen flex flex-col px-2 md:px-16'>
+    <div className='w-full min-h-[70vh] flex flex-col justify-center py-10 md:pb-20 px-2 md:px-16'>
       <span className="w-[2px] inline-block h-16 bg-primary/50 mb-4 md:mb-5"></span>
-      <h2 className='font-poppins text-primary text-lg tracking-wider uppercase md:text-xl lg:text-2xl'>Discover Out Hotel</h2>
-      <div className='flex flex-col items-start justify-between md:flex-row'>
+      <h2 className='font-poppins text-primary text-lg tracking-wider uppercase md:text-xl lg:text-2xl'>Discover Our Hotel</h2>
       <h1 className='font-bellefair text-3xl my-2 md:text-5xl lg:text-6xl'>Enjoy A Luxury Experience</h1>
-      <button className='font-poppins italic text-primary border-t-2 border-b-2 border-primary text-sm py-1 px-7 my-3 lg:my-7 lg:text-base'>Discover More ›</button>
-      </div>
+      <FaqData />
     </div>
   )
 }
--- src/Components/About/FaqData.jsx
+++ src/Components/About/FaqData.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
+
 
 function FaqData() {
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const data = [
         {
             ques: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.`,
@@ -28,11 +32,11 @@ function FaqData() {
         }
     ]
   return (
-    <div className='flex flex-wrap'>
+    <div className='flex flex-wrap justify-between items-start'>
       {data.map((item, index) => (
-        <div key={index}>
-          <h1>{item.ques}</h1>
-          <p>{item.ans}</p>
+        <div key={index} className='w-full mt-7 md:mt-10 md:w-[45%] border-b-2 border-black/30 pb-2'>
+          <h1 className='font-bellefair text-base flex items-center justify-between md:text-xl lg:text-2xl' onClick={() => setActiveIndex(index === activeIndex ? null : index)}>{item.ques} <span>{index === activeIndex ? <FaAngleUp /> : <FaAngleDown />}</span></h1>
+          <p className={`font-poppins text-xs ${index === activeIndex ? 'h-auto' : 'h-0'} overflow-hidden text-black/70 md:text-sm lg:text-base duration-500`}>{item.ans}</p>
         </div>
       ))}
     </div>
