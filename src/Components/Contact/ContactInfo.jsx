9d9b66f7e21afc742b31c818124605f12bff648f
pravesh dav <humantorch1932@gmail.com>
2024-12-20 16:39:30 +0530

    s

--- src/Components/Contact/ContactInfo.jsx
+++ src/Components/Contact/ContactInfo.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { TfiEmail } from "react-icons/tfi";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { MdCall } from "react-icons/md";
+import ContactMap from './ContactMap';
 
 function ContactInfo() {
   return (
     <section className='w-full h-auto bg-white px-1 pt-52 flex flex-col gap-3 md:flex-row md:justify-center md:gap-14 md:pt-28 md:h-[110vh] lg:h-[120vh] lg:pt-36 lg:justify-between lg:gap-0 lg:px-28 xl:px-40 xl:h-[110vh]'>
       <div className='w-full h-80 bg-gray-900 flex-shrink-0 md:w-80 md:h-[33rem] lg:w-96 lg:h-[35rem] xl:w-[28rem]'>
-      <img src="/" className='w-full h-full object-cover' alt="" />
+      <ContactMap />
       </div>
       <div className='py-4 md:w-[45%] lg:-[50%]'>
         <span className="w-[2px] inline-block h-16 bg-primary/50 mb-4 md:mb-5"></span>
